4857cd2502a39ec1abec4db43f18fe1270fc5b25
kebhr <42484226+kebhr@users.noreply.github.com>
2022-07-24 00:31:31 +0900

    feat: support CORS

--- src/main/java/jp/kebihara/programmablesteve/server/CommandHandler.java
+++ src/main/java/jp/kebihara/programmablesteve/server/CommandHandler.java
@@ -11,27 +11,43 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
 public class CommandHandler implements HttpHandler {
-    public void handle(HttpExchange t) throws IOException {
+    public void handle(HttpExchange httpExchange) throws IOException {
         // process request
-        InputStream is = t.getRequestBody();
+        InputStream is = httpExchange.getRequestBody();
         byte[] b = is.readAllBytes();
         is.close();
         String cmd = new String(b);
 
-        // process command
-        String resp = "ok";
-        try {
-            CommandProcessor.process(cmd);
-        } catch (UnknownCommandException | InvalidArgumentException e) {
-            resp = "ng";
-            e.printStackTrace();
+        // CORS
+        httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+
+        String resp = "";
+
+        String method = httpExchange.getRequestMethod();
+        switch (method.toUpperCase()) {
+            case "POST" -> {
+                // process command
+                resp = "ok";
+                try {
+                    CommandProcessor.process(cmd);
+                } catch (UnknownCommandException | InvalidArgumentException e) {
+                    resp = "ng";
+                    e.printStackTrace();
+                }
+            }
+            case "OPTIONS" -> {
+                // CORS
+                httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods", "*");
+                httpExchange.sendResponseHeaders(204, 0);
+                return;
+            }
         }
 
         // make response
-        t.sendResponseHeaders(200, resp.getBytes(StandardCharsets.UTF_8).length);
+        httpExchange.sendResponseHeaders(200, resp.getBytes(StandardCharsets.UTF_8).length);
 
         // return response
-        OutputStream os = t.getResponseBody();
+        OutputStream os = httpExchange.getResponseBody();
         os.write(resp.getBytes());
         os.close();
     }
